daa64a967a53cba4a6314fd60291f30b5a252954
Theodore Byron <ted@tedbyron.com>
2019-08-26 15:44:20 -0400

    Update .gitignore
    update craft -> 3.2.10
    update .gitignore to ignore image transform folders

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,16 +9,16 @@ class ComposerStaticInit531abb82119048e37a570d64920e2432
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
-        '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        'def43f6c87e4f8dfd0c9e1b1bab14fe8' => __DIR__ . '/..' . '/symfony/polyfill-iconv/bootstrap.php',
-        'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         '538ca81a9a966a6716601ecf48f4eaef' => __DIR__ . '/..' . '/opis/closure/functions.php',
+        '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        'def43f6c87e4f8dfd0c9e1b1bab14fe8' => __DIR__ . '/..' . '/symfony/polyfill-iconv/bootstrap.php',
+        'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
+        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '65fec9ebcfbb3cbb4fd0d519687aea01' => __DIR__ . '/..' . '/danielstjules/stringy/src/Create.php',
     );
 
@@ -2589,7 +2589,6 @@ class ComposerStaticInit531abb82119048e37a570d64920e2432
         'craft\\records\\MatrixBlockType' => __DIR__ . '/..' . '/craftcms/cms/src/records/MatrixBlockType.php',
         'craft\\records\\Migration' => __DIR__ . '/..' . '/craftcms/cms/src/records/Migration.php',
         'craft\\records\\Plugin' => __DIR__ . '/..' . '/craftcms/cms/src/records/Plugin.php',
-        'craft\\records\\Route' => __DIR__ . '/..' . '/craftcms/cms/src/records/Route.php',
         'craft\\records\\Section' => __DIR__ . '/..' . '/craftcms/cms/src/records/Section.php',
         'craft\\records\\Section_SiteSettings' => __DIR__ . '/..' . '/craftcms/cms/src/records/Section_SiteSettings.php',
         'craft\\records\\Session' => __DIR__ . '/..' . '/craftcms/cms/src/records/Session.php',
